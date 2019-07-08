79ebc3242fa1f503b4daa4eb4313e1621a95900b
Gaetan Cambier <Gaetan@Cambier.org>
2019-07-08 22:31:52 +0200

    Optimize script

--- docker_crossarch_common.sh
+++ docker_crossarch_common.sh
@@ -73,7 +73,6 @@ crossarch_common_build () {
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
   
   for arch in "${__crossarch_archs[@]}"; do
-    for branch in "${__crossarch_alpine_branch[@]}"; do
       local tmp_dir
       tmp_dir=$(mktemp -d -p /tmp crossarch.XXXXXX)
 
@@ -116,7 +115,6 @@ EOF
     __info "Building ${arch} image..."
     docker build "${build_flags[@]}" --cache-from "gaetancambier/${build_name}:${arch}-latest" -t "build:${arch}" "${tmp_dir}"
     rm -rf "${tmp_dir}"
-    done
   done
 }
 
@@ -148,19 +146,55 @@ crossarch_common_deploy () {
       docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-${build_version_major}"
       docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-${build_version_major}.${build_version_minor}"
       docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-${build_version_major}.${build_version_minor}.${build_version_patch}"
-      docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-latest"
       docker push "gaetancambier/${build_name}:${arch}-${build_version_major}"
       docker push "gaetancambier/${build_name}:${arch}-${build_version_major}.${build_version_minor}"
       docker push "gaetancambier/${build_name}:${arch}-${build_version_major}.${build_version_minor}.${build_version_patch}"
-      docker push "gaetancambier/${build_name}:${arch}-latest"
     else
       docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-${build_version}"
-      docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-latest"
       docker push "gaetancambier/${build_name}:${arch}-${build_version}"
+    fi
+  done
+}
+
+crossarch_common_deploy_latest () {
+  local docker_username="${1}"
+  local docker_password="${2}"
+  local build_name="${3}"
+  local build_version="${4}"
+  
+  local build_version_major=0
+  local build_version_minor=0
+  local build_version_patch=0
+  # shellcheck disable=SC2034
+  local build_version_special=""
+  
+  __info "Deploying ${build_name} (${build_version})..."
+    
+  if [ "${__crossarch_build_is_semver}" = "true" ]; then
+    __crossarch_common_parse_semver "${build_version}" build_version_major build_version_minor build_version_patch build_version_special
+    
+    __info "Version major: ${build_version_major}, minor: ${build_version_minor}, patch: ${build_version_patch}"
+  fi
+  
+  __info "Pushing images to Docker Hub..."
+  docker login -u "${docker_username}" -p "${docker_password}"
+  for arch in "${__crossarch_archs[@]}"; do
+    if [ "${__crossarch_build_is_semver}" = "true" ]; then
+      docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-latest"
+      docker push "gaetancambier/${build_name}:${arch}-latest"
+    else
+      docker tag "build:${arch}" "gaetancambier/${build_name}:${arch}-latest"
       docker push "gaetancambier/${build_name}:${arch}-latest"
     fi
   done
 }
+
+
+
+
+
+
+
 
 
 
@@ -170,15 +204,18 @@ crossarch_common_entry () {
   local docker_password="${3}"
 
   crossarch_common_cache "${build_name}"
-  if [[ -f "./${build_name}/entrypoint.sh" ]]; then
-    crossarch_common_build "${build_name}" "./${build_name}/Dockerfile" "./${build_name}/entrypoint.sh"
-  else
-    crossarch_common_build "${build_name}" "./${build_name}/Dockerfile"
-  fi
-  source "./${build_name}/get_version.sh"
-  local build_version
-  build_version=$(crossarch_build_get_version)
-  crossarch_common_deploy "${docker_username}" "${docker_password}" "${build_name}" "${build_version}"
+  for branch in "${__crossarch_alpine_branch[@]}"; do
+    if [[ -f "./${build_name}/entrypoint.sh" ]]; then
+      crossarch_common_build "${build_name}" "./${build_name}/Dockerfile" "./${build_name}/entrypoint.sh"
+    else
+      crossarch_common_build "${build_name}" "./${build_name}/Dockerfile"
+    fi
+    source "./${build_name}/get_version.sh"
+    local build_version
+    build_version=$(crossarch_build_get_version)
+    crossarch_common_deploy "${docker_username}" "${docker_password}" "${build_name}" "${build_version}"
+  done
+  crossarch_common_deploy_latest "${docker_username}" "${docker_password}" "${build_name}" "${build_version}"
 }
 
 
